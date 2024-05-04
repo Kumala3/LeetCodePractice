7129b23f0ee927270be9ddf357b0c83677a9f7d7
Kostek <ksytnik73kwer@gmail.com>
2024-05-04 20:46:51 +0200

    Add solution to removeElement problem

---
+++ easy/remove_element.py
@@ -0,0 +1,15 @@
+class Solution:
+    """
+    Class representing a solution to the 'removeElement' problem.
+    
+    The 'removeElement' method removes all instances of a given value from a list of integers.
+    It modifies the input list in-place and returns the new length of the list after removal.
+    """
+    def removeElement(self, nums: list[int], val: int) -> int:
+        nums[:] = [d for d in nums if d != val]
+
+        return len(nums)
+
+
+sol = Solution()
+print(sol.removeElement([0, 1, 2, 2, 3, 0, 4, 2], 4))
