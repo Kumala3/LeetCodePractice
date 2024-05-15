11b9645d453b6f4df2677fa550544fc50394c24c
Kostek <ksytnik73kwer@gmail.com>
2024-05-15 23:25:43 +0200

    Add recursive function to calculate gs2 sequence

--- easy/recursive_funcs.py
+++ easy/recursive_funcs.py
@@ -10,6 +10,13 @@ class RecFuncs:
             return 2
         return self.gs1(n - 2) + self.gs1(n - 1) + 2
 
+    def gs2(self, n: int):
+        if n == 1:
+            return 1.5
+        if n == 2:
+            return 1
+        return self.gs2(n - 2) - self.gs2(n - 1) + 3
+
 
 number = int(input("Enter a number please:   "))
 rec_func = RecFuncs()
